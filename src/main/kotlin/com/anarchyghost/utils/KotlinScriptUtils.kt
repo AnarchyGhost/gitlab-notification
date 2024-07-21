139ea94ec1bc955e56a4aeb011834b6769750d4e
AnarchyGhost <k.markelov@anarchyghost.com>
2024-07-21 12:12:07 +0300

    errors

--- src/main/kotlin/com/anarchyghost/utils/KotlinScriptUtils.kt
+++ src/main/kotlin/com/anarchyghost/utils/KotlinScriptUtils.kt
@@ -29,14 +29,14 @@ object DataAccessEvaluator {
             val currentPartValue = if(currentPart.startsWith('[') && currentPart.endsWith(']')) evaluate(event, currentPart.removePrefix("[").removeSuffix("]")).toString() else currentPart
             when (current) {
                 is List<*> -> {
-                    current = (current as List<*>)[currentPartValue.toIntOrNull()!!]!!
+                    current = (current as List<*>)[currentPartValue.toIntOrNull() ?: error("Can't cast currentPartValue $currentPartValue to Int")] ?: error("Part $currentPartValue not found at $current")
                 }
                 is Map<*, *> -> {
-                    current = (current as Map<*, *>)[currentPartValue]!!
+                    current = (current as Map<*, *>)[currentPartValue]   ?: error("Part $currentPartValue not found at $current")
                 }
                 else -> {
-                    current = (current::class as KClass<in Any>).memberProperties.first { it.name == currentPartValue }
-                        .invoke(current)!!
+                    current = ((current::class as KClass<in Any>).memberProperties.firstOrNull { it.name == currentPartValue } ?: error("Mebmer $currentPartValue not found at $current"))
+                        .invoke(current) ?: error("Invoke $currentPartValue at $current returns null")
                 }
             }
         }
